341fffc113717b982426f1caa1271332d6ab6171
Nathan Gerhart <nathan@devresults.com>
2019-09-05 16:42:17 -0400

    let's try shelling to powershell

--- action-a/entrypoint.js
+++ action-a/entrypoint.js
@@ -1,6 +1,6 @@
 // Adapted from https://stackoverflow.com/a/10181488/1172663
 var spawn = require("child_process").spawn,child;
-child = spawn("cmd.exe",["-c", "echo Hello world my name is $MY_NAME"]);
+child = spawn("powershell.exe",["-command", 'echo "Hello world my name is %MY_NAME%"']);
 child.stdout.on("data",function(data){
     console.log("StdOut: " + data);
 });
